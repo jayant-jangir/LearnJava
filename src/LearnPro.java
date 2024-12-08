7a1baf0d93945db6ebc816ad2a590c6d9f78bbba
jayant-jangir <jayant-jangir@outlook.com>
2024-12-08 17:14:22 +0530

    start a fresh

---
+++ src/LearnPro.java
@@ -0,0 +1,3 @@
+public class LearnPro {
+    public String name = "Jayant";
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-
-
+import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello.. you are here now." + args[0]);
-        System.out.println("You are going to be here right now." + args[1]);
 
     }
 }
