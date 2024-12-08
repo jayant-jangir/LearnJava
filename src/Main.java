23ee3257d55fb58ae016b7e6dfbf257c462e625c
jayant-jangir <jayant-jangir@outlook.com>
2024-12-08 08:51:33 +0530

    firstJavaPro.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello.. you are here now." + args[0]);
+        System.out.println("You are going to be here right now." + args[1]);
+
+    }
+}
