00e5555280dc84b4f1770af1680241c5d2b57d61
Jayant Jangir <jayant-jangir@outlook.com>
2025-01-11 22:10:59 +0530

    Permutation and Combination

---
+++ src/Combination.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Combination {
+    public static void main(String[] args) {
+        String comb = "";
+        String input = "abc";
+
+        ArrayList<String> combinations = combination(comb, input);
+        System.out.println(combinations);
+    }
+
+    // Combinations
+    private static ArrayList<String> combination(String comb, String input) {
+        ArrayList<String> combinations = new ArrayList<String>();
+        if (input.isEmpty()) {
+            combinations.add(comb);
+            return combinations;
+        }
+
+        combinations.addAll(combination(comb, input.substring(1)));
+        combinations.addAll(combination(comb + input.charAt(0), input.substring(1)));
+
+        return combinations;
+    }
+}
